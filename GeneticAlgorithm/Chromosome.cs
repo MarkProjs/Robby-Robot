678bfe9b60e54501498e73a2f5404b0050ae004e
Maedeh Hassani <maedeh.hassani@dawsoncollege.qc.ca>
2022-11-01 22:36:02 -0400

    improve the chromosome class

--- GeneticAlgorithm/Chromosome.cs
+++ GeneticAlgorithm/Chromosome.cs
@@ -5,11 +5,10 @@ namespace GeneticLibrary
 {
   public class Chromosome : IChromosome
   {
-    private Random rand;
 
-    public Chromosome(int numberOfGenes, int LengthOfGene, int seed = 0){
+    public Chromosome(int numberOfGenes, int LengthOfGene, int? seed = null){
       Genes = new int[numberOfGenes];
-      rand = new Random(seed);
+      var rand = new Random(seed.GetValueOrDefault());
       for (int i = 0; i < numberOfGenes; i++)
       {
         int rndInt = rand.Next(LengthOfGene);
@@ -22,6 +21,10 @@ namespace GeneticLibrary
     public Chromosome(IChromosome choromosome){
       Fitness = choromosome.Fitness;
       Genes = new int[choromosome.Length];
+      for (var i = 0; i < choromosome.Length; i++)
+      {
+        Genes[i] = choromosome[i];
+      } 
     }
 
     //implement indxer
@@ -32,23 +35,24 @@ namespace GeneticLibrary
 
     
     public int CompareTo([AllowNull] IChromosome other) {
-      if (Fitness == other.Fitness) {
-        return 0;
-      }
-      else if (Fitness > other.Fitness) {
-        return 1;
-      }
-      else {
-        return -1;
-      }
+      return Fitness.CompareTo(other.Fitness);
+      //return other.Fitness.CompareTo(Fitness);
+      
+      // if (Fitness == other.Fitness) {
+      //   return 0;
+      // }
+      // else if (Fitness > other.Fitness) {
+      //   return 1;
+      // }
+      // else {
+      //   return -1;
+      // }
     }
 
     public IChromosome[] Reproduce(IChromosome spouse, double mutationProb){
-      rand = new Random();
+      var rand = new Random();
       int pointA = rand.Next((int)Length);
       int pointB = rand.Next(pointA, (int)Length);
-      // int[] p1Tmp = spouse.Genes[pointA..pointB]; //p1
-      // int[] p2Tmp = this.Genes[pointA..pointB]; //p2
 
       for (int i = pointA; i < pointB; i++) {
           double mutation = rand.NextDouble();
@@ -62,18 +66,7 @@ namespace GeneticLibrary
           spouse.Genes[i] = tmp;
         }
       }
-      // mutation of the child
-      // double rndDouble = rand.NextDouble();
-      // for (int i = 0; i < Genes.Length; i++)
-      // {
-      //   if (mutationProb >= rndDouble)
-      //   {
-      //     int rndInt = rand.Next((int)Length);
-      //   }
-      // }
       return new []{this, spouse};
-            
-
       }
 
     public double Fitness { get; set; }
@@ -81,7 +74,7 @@ namespace GeneticLibrary
     public int[] Genes { get; }
 
     // The Length = 243 
-    public long Length { get; }
+    public long Length { get { return Genes.Length; } }
 
 }
 }
