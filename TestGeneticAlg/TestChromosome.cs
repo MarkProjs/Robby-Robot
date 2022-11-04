963df5211e5b9eae284f2c27c91b8ca1fa4a0b4a
Mark Agluba <mark.agluba@dawsoncollege.qc.ca>
2022-11-04 11:18:28 -0400

    tested the length, fitness, and the genes of the chromosome class

--- TestGeneticAlg/TestChromosome.cs
+++ TestGeneticAlg/TestChromosome.cs
@@ -1,11 +1,12 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using GeneticLibrary;
+using System;
 namespace TestGeneticAlg
 {
     [TestClass]
     public class UnitTest1
     {   
-        Chromosome chr = new Chromosome(5, 7);
+        Chromosome chr = new Chromosome(5, 7, 1);
 
         [TestMethod]
         public void TestFitness()
@@ -17,6 +18,20 @@ namespace TestGeneticAlg
         public void TestLength()
         {
             Assert.AreEqual(chr.Length, 5);
+        }
+
+        [TestMethod]
+        public void TestGenes()
+        {
+            Random rnd = new Random(1);
+            int[] tempGenes = new int[5];
+            for (int i = 0; i < 5; i++)
+            {
+                int rndInt = rnd.Next(7);
+                tempGenes[i] = rndInt;
+            } 
+
+            Assert.AreEqual(chr[0], tempGenes[0]);
         }
     }
 }
