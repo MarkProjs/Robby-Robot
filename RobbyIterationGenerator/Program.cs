6c7d185f13f68909da13954f8cbfc31a3555d76c
Mark Agluba <mark.agluba@dawsoncollege.qc.ca>
2022-11-08 23:04:44 -0500

    changing the condition of the while becasue it was making 51 cans instead of 50, as well as testing it in the Program

--- RobbyIterationGenerator/Program.cs
+++ RobbyIterationGenerator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using RobbyTheRobot;
 
 namespace RobbyIterationGenerator
 {
@@ -6,18 +7,30 @@ namespace RobbyIterationGenerator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+        //     Console.WriteLine("Hello World!");
 
-            int[] list = {98, 23, 97, 36, 77};
-         Console.WriteLine("Original Unsorted List");
-         foreach (int i in list) {
-            Console.Write(i + " ");
-         }
-         Array.Sort(list);
-         Console.WriteLine("sorted List");
-         for(int i=0; i<list.Length; i++) {
-            Console.Write(list[i] + " ");
-         }
+        //     int[] list = {98, 23, 97, 36, 77};
+        //  Console.WriteLine("Original Unsorted List");
+        //  foreach (int i in list) {
+        //     Console.Write(i + " ");
+        //  }
+        //  Array.Sort(list);
+        //  Console.WriteLine("sorted List");
+        //  for(int i=0; i<list.Length; i++) {
+        //     Console.Write(list[i] + " ");
+        //  }
+        IRobbyTheRobot rob = Robby.CreateRobby(2, 2, 2);
+        ContentsOfGrid[,] tempGrid = rob.GenerateRandomTestGrid();
+        int count = 0;
+        foreach(ContentsOfGrid elem in tempGrid) 
+        {
+            if (elem == ContentsOfGrid.Can)
+            {
+                count +=1;
+            }
+        }
+        Console.WriteLine(count);
+        
 
         }
     }
