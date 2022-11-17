1ea6249afbf66996e8f465d570300e40c2b6439d
Maedeh Hassani <maedeh.hassani@dawsoncollege.qc.ca>
2022-11-17 11:12:54 -0500

    add some new test cases

--- TestGeneticAlg/generation.cs
+++ TestGeneticAlg/generation.cs
@@ -1,25 +1,35 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using GeneticLibrary;
+// using Microsoft.VisualStudio.TestTools.UnitTesting;
+// using GeneticLibrary;
 
-namespace TestGeneticAlg
-{
-  [TestMethod]
-  public class generation
-  {
-    // public void TestDefaultArrayLength()
-    static Chromosome chromosome01 = new Chromosome(1, 2,3);
-    static Chromosome chromosome02 = new Chromosome(1, 2,3);
+// namespace TestGeneticAlg
+// {
+//   [TestClass]
+//   public class generation
+//   {
+//     // public void TestDefaultArrayLength()
+//     static Chromosome chromosome01 = new Chromosome(1, 2,3);
+//     static Chromosome chromosome02 = new Chromosome(1, 2,3);
+//     static Chromosome[] chromosomeArray = new Chromosome[] { chromosome01 };
 
-    public static double fitnes(IChromosome chromosome, IGeneration generation){
-      return 120;
-    }
-    public static GeneticAlgorithm _geneticAlgorithm = new GeneticAlgorithm(200, 247, 10, 2,  4, 100, fitnes, 10);
-    public static IGeneration _generation = new Generation(_geneticAlgorithm , fitnes, 0);
+//     public static double fitnes(IChromosome chromosome, IGeneration generation){
+//       return 120;
+//     }
+//     public static GeneticAlgorithm _geneticAlgorithm = new GeneticAlgorithm(200, 247, 10, 2,  4, 100, fitnes, 10);
+//     public static IGeneration _generation = new Generation(_geneticAlgorithm , fitnes, 0);
 
-    [TestMethod]
-        public void TestGeneration()
-        {
-          Assert.AreEqual( );
-        }
-  }
-}
+// //?????????????????????????????????????
+//     [TestMethod]
+//       public void TestGeneration()
+//       {
+//         Generation gen = new Generation(chromosomeArray);
+//         Assert.AreEqual(gen, chromosome01);
+//       }
+
+// //?????????????????????????????
+//     [TestMethod]
+//       public void TestNumberOfChromosomes()
+//       {
+//         Assert.AreEqual(_generation.NumberOfChromosomes, 1);
+//       }
+//   }
+// }
